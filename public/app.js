3413e145dd8709865019e9d3ae43ed9df09fdcad
Sean McConnell <seanmac11741@gmail.com>
2024-10-28 11:42:04 -0500

    save state for darkmode

--- public/app.js
+++ public/app.js
@@ -1,2 +1,31 @@
+// Dark mode
 const toggle = document.getElementById("toggle");
-toggle.addEventListener("click", () => { document.body.classList.toggle("dark-mode"); });
+let modeText = document.querySelector(".mode-text");
+const localDarkMode = localStorage.getItem("darkMode");
+if (localDarkMode === "false") {
+    light();
+} else {
+    dark();
+}
+toggle.addEventListener("click", toggleMode);
+function toggleMode() {
+    if (toggle.checked) {
+        dark();
+    } else {
+        light();
+    }
+}
+
+function dark() {
+    toggle.checked = true;
+    document.body.classList.add("dark-mode");
+    modeText.innerText = "Dark";
+    localStorage.setItem("darkMode", "true");
+}
+
+function light() {
+    toggle.checked = false;
+    document.body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", "false");
+    modeText.innerText = "Light";
+}
